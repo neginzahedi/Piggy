6a79e838b3f03251ac5336568241d8a3397946ba
Negin Zahedi <zahedi.negin@gmail.com>
2024-03-14 23:44:19 -0400

    Add MARK comments and placeholder views to ContentView

--- Piggy/ContentView.swift
+++ Piggy/ContentView.swift
@@ -7,49 +7,41 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    
+    // MARK: Properties
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Transaction]
+    @Query private var transactions: [Transaction]
     
+    // MARK: Body
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("\(item.title)")
-                    } label: {
-                        Text("\(item.title)")
-                        
+        NavigationStack {
+            VStack{
+                CardView(title: "Total Balance", subTitle: "$3504.09", cardViewColor: Color.purple.opacity(0.5))
+                    .padding(.horizontal, 20)
+                    .padding(.top, 10)
+                HStack(spacing: 10){
+                    CardView(title: "Income", subTitle: "$3504.09", cardViewColor: Color.yellow.opacity(0.5))
+                    CardView(title: "Expense", subTitle: "$3504.09", cardViewColor: Color.green.opacity(0.5))
+                }
+                .padding(.horizontal, 20)
+                .padding(.top, 10)
+                
+                VStack{
+                    HStack{
+                        Text("Recent Transaction")
+                        Spacer()
+                    }
+                    ScrollView{
+                        ForEach(transactions) { transaction in
+                            RecentTransactionView(transaction: transaction)
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .padding()
+                Spacer()
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-    
-    private func addItem() {
-        withAnimation {
-            //            let newItem = Transaction(timestamp: Date())
-            //            modelContext.insert(newItem)
-        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            //            for index in offsets {
-            //                modelContext.delete(items[index])
-            //            }
+            .navigationTitle("Dashboard")
+            .fontDesign(.rounded)
         }
     }
 }
@@ -57,4 +49,50 @@ struct ContentView: View {
 #Preview {
     ContentView()
         .modelContainer(for: Transaction.self, inMemory: true)
+}
+
+// MARK: - CardView
+struct CardView: View {
+    
+    // MARK: Properties
+    var title: String
+    var subTitle: String
+    var cardViewColor: Color
+    
+    // MARK: Body
+    var body: some View {
+        VStack{
+            Text(title)
+            Text(subTitle)
+        }
+        .bold()
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 40)
+        .background(cardViewColor)
+        .cornerRadius(10)
+    }
+}
+
+// MARK: - RecentTransactionView
+struct RecentTransactionView: View {
+    
+    // MARK: Properties
+    var transaction: Transaction
+    
+    // MARK: Body
+    var body: some View {
+        NavigationLink {
+            Text("\(transaction.title)")
+        } label: {
+            HStack{
+                Text("\(transaction.title)")
+            }
+            .bold()
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 10)
+            .background(Color.blue.opacity(0.5))
+            .cornerRadius(10)
+            .foregroundStyle(.black)
+        }
+    }
 }
